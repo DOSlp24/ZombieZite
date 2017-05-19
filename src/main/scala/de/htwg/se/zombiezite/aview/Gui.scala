2022971fccfa26d8080c415b0b416ff3bb4f2bdb
da241die <da241die@3dlab3.in.fhkn.de>
2017-05-19 13:00:42 +0200

    a

--- src/main/scala/de/htwg/se/zombiezite/aview/Gui.scala
+++ src/main/scala/de/htwg/se/zombiezite/aview/Gui.scala
@@ -10,7 +10,30 @@ import scala.swing.event._
 import scala.io.Source._
 
 class Gui(c: Controller) extends Frame {
-  
 
-  visible = true
+  title = "Zombie Zite"
+
+  title = "Hello, World!"
+
+  val label =
+    new Label {
+      text = "Hello World"
+      font = new Font("Verdana", 1, 36)
+    }
+  
+  menuBar = new MenuBar {
+    contents += new Menu("File") {
+      mnemonic = Key.F
+      contents += new MenuItem(Action("New") { System.console().flush() })
+      contents += new MenuItem(Action("Quit") { System.exit(0) })
+    }
+  }
+
+  val cell = new BoxPanel(Orientation.Vertical) {
+    contents += label
+  }
+
+  contents = menuBar
+
+    visible = true
 }
