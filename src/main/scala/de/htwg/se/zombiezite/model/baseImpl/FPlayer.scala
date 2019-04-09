77e66aff32b829319a5cac41f18b303751dcd14e
Daniel Dietze <daniel.dietze@bmtg.ch>
2019-04-09 16:49:12 +0200

    - Item Drop on use implemented

--- src/main/scala/de/htwg/se/zombiezite/model/baseImpl/FPlayer.scala
+++ src/main/scala/de/htwg/se/zombiezite/model/baseImpl/FPlayer.scala
@@ -3,20 +3,20 @@ package de.htwg.se.zombiezite.model.baseImpl
 import de.htwg.se.zombiezite.model._
 
 case class FPlayer(
-    override val x: Int,
-    override val y: Int,
-    override val lifePoints: Int = 100,
-    override val armor: Int = 0,
-    override val strength: Int = 1,
-    override val range: Int = 0,
-    override val name: String = "Player",
-    override val actionCounter: Int = 3,
-    override val equipment: Array[FItemInterface] = Array[FItemInterface](),
-    override val equippedWeapon: FWeaponInterface = FWeapon("Fist", 1, 0)
-) extends FPlayerInterface {
+                    override val x: Int,
+                    override val y: Int,
+                    override val lifePoints: Int = 100,
+                    override val armor: Int = 0,
+                    override val strength: Int = 1,
+                    override val range: Int = 0,
+                    override val name: String = "Player",
+                    override val actionCounter: Int = 3,
+                    override val equipment: Array[FItemInterface] = Array[FItemInterface](),
+                    override val equippedWeapon: FWeaponInterface = FWeapon("Fist", 1, 0)
+                  ) extends FPlayerInterface {
 
   override def useArmor(a: FArmorInterface): FPlayerInterface = {
-    copy(armor = armor + a.protection)
+    copy(armor = armor + a.protection).drop(a)
   }
 
   override def equipWeapon(weapon: FWeaponInterface): FPlayerInterface = {
