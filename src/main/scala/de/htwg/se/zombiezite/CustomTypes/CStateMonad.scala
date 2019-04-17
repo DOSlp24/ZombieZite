51a2d4bae26a3315d4c96ec34f1aafd5583a2c39
Daniel Dietze <daniel.dietze@bmtg.ch>
2019-04-17 08:36:39 +0200

    - CStateMonad implemented

---
+++ src/main/scala/de/htwg/se/zombiezite/CustomTypes/CStateMonad.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.zombiezite.CustomTypes
+
+import de.htwg.se.zombiezite.controller.cState
+
+class CStateMonad[T](val states: Seq[cState]) {
+  def map(s:T => T): Seq[T] = states.map(state => s(state))
+}
+
+trait Option[cState] {
+  def map(f: cState => cState): Option[cState]
+}
+
+case class Some[cState](b: cState) extends Option[cState] {
+  def map(f: cState => cState) = Some(f(b))
+}
+
+case class None[cState]() extends Option[cState] {
+  def map(f: cState => cState) = new None
+}
